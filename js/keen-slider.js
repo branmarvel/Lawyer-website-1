41e638db71489d8e5cd1b0ef5c18588327d1de74
branmarvel <88461465+branmarvel@users.noreply.github.com>
2022-07-04 12:09:51 -0400

    Agregado secciones y carousel

--- js/keen-slider.js
+++ js/keen-slider.js
@@ -184,6 +184,99 @@ function navigation2(slider) {
     markup(true);
   });
 }
+function navigation3(slider) {
+  let wrapper, dots, arrowLeft, arrowRight;
+
+  function markup(remove) {
+    wrapperMarkup(remove);
+    dotMarkup(remove);
+    arrowMarkup(remove);
+  }
+
+  function removeElement(elment) {
+    elment.parentNode.removeChild(elment);
+  }
+  function createDiv(className) {
+    var div = document.createElement("div");
+    var classNames = className.split(" ");
+    classNames.forEach((name) => div.classList.add(name));
+    return div;
+  }
+
+  function arrowMarkup(remove) {
+    if (remove) {
+      removeElement(arrowLeft);
+      removeElement(arrowRight);
+      return;
+    }
+    arrowLeft = createDiv("arrow-3 arrow--left-3");
+    arrowLeft.addEventListener("click", () => slider.prev());
+    arrowRight = createDiv("arrow-3 arrow--right-3");
+    arrowRight.addEventListener("click", () => slider.next());
+
+    wrapper.appendChild(arrowLeft);
+    wrapper.appendChild(arrowRight);
+  }
+
+  function wrapperMarkup(remove) {
+    if (remove) {
+      var parent = wrapper.parentNode;
+      while (wrapper.firstChild)
+        parent.insertBefore(wrapper.firstChild, wrapper);
+      removeElement(wrapper);
+      return;
+    }
+    wrapper = createDiv("navigation-wrapper-2");
+    slider.container.parentNode.appendChild(wrapper);
+    wrapper.appendChild(slider.container);
+  }
+
+  function dotMarkup(remove) {
+    if (remove) {
+      removeElement(dots);
+      return;
+    }
+    dots = createDiv("dots");
+    slider.track.details.slides.forEach((_e, idx) => {
+      var dot = createDiv("dot");
+      dot.addEventListener("click", () => slider.moveToIdx(idx));
+      dots.appendChild(dot);
+    });
+    wrapper.appendChild(dots);
+  }
+
+  function updateClasses() {
+    var slide = slider.track.details.rel;
+    slide === 0
+      ? arrowLeft.classList.add("arrow--disabled-3")
+      : arrowLeft.classList.remove("arrow--disabled-3");
+    slide === slider.track.details.slides.length - 1
+      ? arrowRight.classList.add("arrow--disabled-3")
+      : arrowRight.classList.remove("arrow--disabled-3");
+    Array.from(dots.children).forEach(function (dot, idx) {
+      idx === slide
+        ? dot.classList.add("dot--active-2")
+        : dot.classList.remove("dot--active-2");
+    });
+  }
+
+  slider.on("created", () => {
+    markup();
+    updateClasses();
+  });
+  slider.on("optionsChanged", () => {
+    console.log(2);
+    markup(true);
+    markup();
+    updateClasses();
+  });
+  slider.on("slideChanged", () => {
+    updateClasses();
+  });
+  slider.on("destroyed", () => {
+    markup(true);
+  });
+}
 var slider = new KeenSlider("#my-keen-slider", {}, [navigation]);
 var slider = new KeenSlider(
   "#my-keen-slider",
@@ -228,8 +321,11 @@ var slider = new KeenSlider(
     slides: {
       perView: 3,
       spacing: 25,
+      slides: { perView: "auto" },
     },
     loop: true,
+    rtl: false,
+    mode: "snap",
   },
   [
     (slider) => {
@@ -263,5 +359,51 @@ var slider = new KeenSlider(
       slider.on("updated", nextTimeout);
     },
     navigation2,
+  ]
+);
+
+var slider = new KeenSlider(
+  "#my-keen-slider-3",
+  {
+    slides: {
+      perView: 4,
+      spacing: 20,
+    },
+    loop: true,
+    rtl: false,
+    mode: "snap",
+  },
+  [
+    (slider) => {
+      let timeout;
+      let mouseOver = false;
+      function clearNextTimeout() {
+        clearTimeout(timeout);
+      }
+      function nextTimeout() {
+        clearTimeout(timeout);
+        if (mouseOver) return;
+        timeout = setTimeout(() => {
+          slider.next();
+        }, 2000);
+      }
+
+      slider.on("created", () => {
+        slider.container.addEventListener("mouseover", () => {
+          mouseOver = true;
+          clearNextTimeout();
+        });
+
+        slider.container.addEventListener("mouseout", () => {
+          mouseOver = false;
+          nextTimeout();
+        });
+        nextTimeout();
+      });
+      slider.on("dragStarted", clearNextTimeout);
+      slider.on("animationEnded", nextTimeout);
+      slider.on("updated", nextTimeout);
+    },
+    navigation3,
   ]
 );
