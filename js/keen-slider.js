a3d453c3828565d4d43322220b47fa9054301f14
branmarvel <88461465+branmarvel@users.noreply.github.com>
2022-07-07 12:52:56 -0400

    solucionado responsive carousel, agregado seccion testimonial, empezado el footer

--- js/keen-slider.js
+++ js/keen-slider.js
@@ -277,6 +277,99 @@ function navigation3(slider) {
     markup(true);
   });
 }
+function navigation4(slider) {
+  let wrapper, dots, arrowLeft, arrowRight;
+
+  function markup(remove) {
+    wrapperMarkup(remove);
+    dotMarkup(remove);
+    arrowMarkup(remove);
+  }
+
+  function removeElement(elment) {
+    elment.parentNode.removeChild(elment);
+  }
+  function createDiv(className) {
+    var div = document.createElement("div");
+    var classNames = className.split(" ");
+    classNames.forEach((name) => div.classList.add(name));
+    return div;
+  }
+
+  function arrowMarkup(remove) {
+    if (remove) {
+      removeElement(arrowLeft);
+      removeElement(arrowRight);
+      return;
+    }
+    arrowLeft = createDiv("arrow-4 arrow--left-4");
+    arrowLeft.addEventListener("click", () => slider.prev());
+    arrowRight = createDiv("arrow-4 arrow--right-4");
+    arrowRight.addEventListener("click", () => slider.next());
+
+    wrapper.appendChild(arrowLeft);
+    wrapper.appendChild(arrowRight);
+  }
+
+  function wrapperMarkup(remove) {
+    if (remove) {
+      var parent = wrapper.parentNode;
+      while (wrapper.firstChild)
+        parent.insertBefore(wrapper.firstChild, wrapper);
+      removeElement(wrapper);
+      return;
+    }
+    wrapper = createDiv("navigation-wrapper");
+    slider.container.parentNode.appendChild(wrapper);
+    wrapper.appendChild(slider.container);
+  }
+
+  function dotMarkup(remove) {
+    if (remove) {
+      removeElement(dots);
+      return;
+    }
+    dots = createDiv("dots-4");
+    slider.track.details.slides.forEach((_e, idx) => {
+      var dot = createDiv("dot-4");
+      dot.addEventListener("click", () => slider.moveToIdx(idx));
+      dots.appendChild(dot);
+    });
+    wrapper.appendChild(dots);
+  }
+
+  function updateClasses() {
+    var slide = slider.track.details.rel;
+    slide === 0
+      ? arrowLeft.classList.add("arrow--disabled-4")
+      : arrowLeft.classList.remove("arrow--disabled-4");
+    slide === slider.track.details.slides.length - 1
+      ? arrowRight.classList.add("arrow--disabled-4")
+      : arrowRight.classList.remove("arrow--disabled-4");
+    Array.from(dots.children).forEach(function (dot, idx) {
+      idx === slide
+        ? dot.classList.add("dot--active-4")
+        : dot.classList.remove("dot--active-4");
+    });
+  }
+
+  slider.on("created", () => {
+    markup();
+    updateClasses();
+  });
+  slider.on("optionsChanged", () => {
+    console.log(2);
+    markup(true);
+    markup();
+    updateClasses();
+  });
+  slider.on("slideChanged", () => {
+    updateClasses();
+  });
+  slider.on("destroyed", () => {
+    markup(true);
+  });
+}
 var slider = new KeenSlider("#my-keen-slider", {}, [navigation]);
 var slider = new KeenSlider(
   "#my-keen-slider",
@@ -429,5 +522,66 @@ var slider = new KeenSlider(
       slider.on("updated", nextTimeout);
     },
     navigation3,
+  ]
+);
+
+var slider = new KeenSlider(
+  "#my-keen-slider-4",
+  {
+    breakpoints: {
+      "(min-width: 400px)": {
+        loop: true,
+        slides: { perView: 1, spacing: 10 },
+      },
+      "(min-width: 576px)": {
+        loop: true,
+        slides: { perView: 2, spacing: 20 },
+      },
+      "(min-width: 768px)": {
+        loop: true,
+        slides: { perView: 3, spacing: 20 },
+      },
+      "(min-width: 1000px)": {
+        loop: true,
+        slides: { perView: 4, spacing: 20 },
+      },
+    },
+
+    loop: true,
+    rtl: false,
+    mode: "snap",
+  },
+  [
+    (slider) => {
+      let timeout;
+      let mouseOver = false;
+      function clearNextTimeout() {
+        clearTimeout(timeout);
+      }
+      function nextTimeout() {
+        clearTimeout(timeout);
+        if (mouseOver) return;
+        timeout = setTimeout(() => {
+          slider.next();
+        }, 2000);
+      }
+
+      slider.on("created", () => {
+        slider.container.addEventListener("mouseover", () => {
+          mouseOver = true;
+          clearNextTimeout();
+        });
+
+        slider.container.addEventListener("mouseout", () => {
+          mouseOver = false;
+          nextTimeout();
+        });
+        nextTimeout();
+      });
+      slider.on("dragStarted", clearNextTimeout);
+      slider.on("animationEnded", nextTimeout);
+      slider.on("updated", nextTimeout);
+    },
+    navigation4,
   ]
 );
